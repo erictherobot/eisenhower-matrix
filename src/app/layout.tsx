634d843e0cd560af250103b91db847c08315cf7d
Eric David Smith <erictherobot@gmail.com>
2023-06-16 15:50:25 -0400

    Yay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/Header";
 import "./globals.css";
+import Header from "@/components/Header";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
